142ed6a2ff187a67b86a0a0c850af1b6ff8cde3c
ikerlopez11 <ikerloma.il@gmail.com>
2024-04-27 17:44:12 +0200

    Done

--- bot.js
+++ bot.js
@@ -2,15 +2,26 @@ function findUserName(initData) {
     let userTitle = document.getElementById("userName");
     console.log(userTitle)
 
-    userTitle.textContent = initData.user.username;
+    userTitle.textContent += initData.user.username;
 }
 
+function setupMainButton(mainButton) {
+    mainButton.setText("Accept");
+    if(!mainButton.isVisible) {
+        mainButton.show();
+    }
+
+    mainButton.onClick(function() {
+        console.log("El lili es crack");
+    })
+}
 
 function run() {
+    const mainButton = window.Telegram.WebApp.MainButton;
     const initData = window.Telegram.WebApp.initDataUnsafe;
     findUserName(initData)
+    setupMainButton(mainButton)
 }
-
 
 
 if(window.Telegram.WebApp) {
